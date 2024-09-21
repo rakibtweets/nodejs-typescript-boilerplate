7516ce12371e8acdaf622892c2cbae1dc16f7a0c
Rakib Hasan <rakibhasanroky0@gmail.com>
2024-09-21 15:24:43 +0600

    Update eslint.config.mjs and package.json
    - Add eslint-plugin-prettier and eslint-config-prettier dependencies
    - Configure eslint rules for prettier formatting
    - Update package.json with prettier dependency

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default [
   {
@@ -10,9 +11,22 @@ export default [
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
   {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off'
+      '@typescript-eslint/no-explicit-any': 'off',
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: true,
+          // parser: 'flow',
+          semi: true,
+          trailingComma: 'none',
+          bracketSameLine: false,
+          bracketSpacing: true,
+          endOfLine: 'auto'
+        }
+      ]
     }
   }
 ];
